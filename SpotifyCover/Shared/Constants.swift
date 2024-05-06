830b9a6396385e6130c7a37188fb6e04773bbfbc
lophiester <gilcharles2022@hotmail.com>
2024-05-06 16:45:17 +0900

    ImageLoaderView created using: SDwebImageSwiftUI package

---
+++ SpotifyCover/Shared/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  SpotifyCover
+//
+//  Created by Charles Yamamoto on 2024/05/06.
+//
+
+import Foundation
---
+++ SpotifyCover/Shared/ImageLoaderView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageLoaderView.swift
+//  SpotifyCover
+//
+//  Created by Charles Yamamoto on 2024/05/06.
+//
+
+import SwiftUI
+
+struct ImageLoaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageLoaderView()
+}
