4ca4e840ba863f72d313140985a5425effd292e3
lophiester <gilcharles2022@hotmail.com>
2024-05-06 16:24:14 +0900

    Initial Commit

---
+++ SpotifyCover/SpotifyCoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCoverApp.swift
+//  SpotifyCover
+//
+//  Created by Charles Yamamoto on 2024/05/06.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
