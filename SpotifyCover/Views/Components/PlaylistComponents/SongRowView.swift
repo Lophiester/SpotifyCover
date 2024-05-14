f8ac0ce11ef443025b69a67c8832632b7ed8f2a7
lophiester <gilcharles2022@hotmail.com>
2024-05-14 16:51:55 +0900

    PlaylistView with SongRowView

---
+++ SpotifyCover/Views/Components/PlaylistComponents/SongRowView.swift
@@ -0,0 +1,70 @@
+//
+//  SonRowView.swift
+//  SpotifyCover
+//
+//  Created by Charles Yamamoto on 2024/05/14.
+//
+
+import SwiftUI
+
+struct SongRowView: View {
+    
+    var urlImage = Constants.randomImage
+    var imageSize: CGFloat = 50
+    var title: String? = "Title goes here"
+    var subtitle: String? = "Subtitle goes here"
+    var onRowPressed: (()-> Void)? = nil
+    var onEllipsisPreesed: (()-> Void)? = nil
+  
+    
+    
+    
+    var body: some View {
+        
+        HStack(spacing: 8){
+                ImageLoaderView(urlImage: urlImage)
+                    .frame(width: imageSize, height: imageSize)
+                VStack(alignment: .leading, spacing: 4){
+                    if let title {
+                        Text(title)
+                            .font(.body)
+                            .fontWeight(.medium)
+                            .foregroundStyle(.spotifyWhite)
+                    }
+                     
+                    if let subtitle {
+                        Text(subtitle)
+                            .font(.callout)
+                            .foregroundStyle(.spotifyLightGray)
+                    }
+                    
+                }
+                .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+             
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                Image(systemName: "ellipsis")
+                    .font(.subheadline)
+                    .foregroundStyle(.spotifyWhite)
+                    .opacity(16)
+                    .background(.black.opacity(0.001))
+                    .onTapGesture {
+                      onEllipsisPreesed?()
+                    }
+            }
+        .onTapGesture {
+            onRowPressed?()
+        }
+        
+    }
+}
+
+#Preview {
+    ZStack{
+        Color.black.ignoresSafeArea()
+        VStack{
+            SongRowView()
+            SongRowView()
+            SongRowView()
+        }
+    }
+}
--- SpotifyCover/Views/PlayList/PlaylistView.swift
+++ SpotifyCover/Views/PlayList/PlaylistView.swift
@@ -11,6 +11,7 @@ struct PlaylistView: View {
     
     @State var product: Product = .mockProduct
     @State var user: User = .mockUser
+    @State var products:[Product] = []
     
     var body: some View {
         ZStack{
@@ -29,14 +30,34 @@ struct PlaylistView: View {
                             userName: user.firstName,
                             subheadline: product.category)
                         .padding(.horizontal,16)
-                        
-                        
+                        ForEach(products){ product in
+                            SongRowView(
+                                urlImage: product.firstImage,
+                                imageSize: 60,
+                                title: product.title,
+                                subtitle: product.brand
+                            )
+                            .padding(.horizontal,16)
+                        }
                     })
             }
-            
+            .scrollIndicators(.hidden)
+            .task {
+               await  getdata()
+            }
         }
         
     }
+    
+   private  func getdata() async {
+        do {
+            products = try await DataBaseHelper().getProducts()
+        } catch  {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
 }
 
 #Preview {
