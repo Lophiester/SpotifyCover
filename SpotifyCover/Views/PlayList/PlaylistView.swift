38af782cd246f84835e00e2b1170c607bc3e5357
lophiester <gilcharles2022@hotmail.com>
2024-05-15 12:02:29 +0900

    PlaylistView updated

--- SpotifyCover/Views/PlayList/PlaylistView.swift
+++ SpotifyCover/Views/PlayList/PlaylistView.swift
@@ -6,12 +6,15 @@
 //
 
 import SwiftUI
+import SwiftfulUI
 
 struct PlaylistView: View {
     
     @State var product: Product = .mockProduct
     @State var user: User = .mockUser
     @State var products:[Product] = []
+    @State var isShowHeader: Bool = false
+    @State var offset: CGFloat = 0
     
     var body: some View {
         ZStack{
@@ -25,6 +28,15 @@ struct PlaylistView: View {
                             subtitle: product.brand,
                             urlImage: product.thumbnail
                         )
+                        //that's being used to calculate an offset. This property is being used to determine the maximum position of the frame (frame.maxY). The code then prints this offset.
+                        .readingFrame { frame in
+                            //offset = frame.maxY
+                           // print(offset)
+                            
+                            isShowHeader = frame.maxY < 150
+                        }
+                        
+                        
                         PlaylistDescriptionView(
                             descriptionText: product.description,
                             userName: user.firstName,
@@ -42,14 +54,39 @@ struct PlaylistView: View {
                     })
             }
             .scrollIndicators(.hidden)
+            ZStack{
+                Text(product.title)
+                    .font(.headline)
+                    .padding(.vertical, 20)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .background(.spotifyBlack)
+                    .offset(y: isShowHeader ? 0 : -40)
+                    .opacity(isShowHeader ? 1 : 0)
+                
+                Image(systemName: "chevron.left")
+                    .font(.title3)
+                    .padding(10)
+                    .background(isShowHeader ? .spotifyGray.opacity(0.001) :.spotifyGray.opacity(0.7))
+                    .clipShape(Circle())
+                    .padding(.leading,16)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                    .onTapGesture {
+                        
+                    }
+                
+            }
+            .foregroundStyle(.spotifyWhite)
+            .animation(.smooth(duration: 0.2), value: isShowHeader)
+            .frame(maxHeight: .infinity, alignment: .top)
+            
             .task {
-               await  getdata()
+                await  getdata()
             }
         }
         
     }
     
-   private  func getdata() async {
+    private  func getdata() async {
         do {
             products = try await DataBaseHelper().getProducts()
         } catch  {
